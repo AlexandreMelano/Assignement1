cbbdd672d1a2ead9585ed26cdce37ae6db92f156
AlexandreMelano <alexandremelano@yahoo.com>
2017-02-20 16:49:01 -0500

    Added php

--- database.php
+++ database.php
@@ -6,12 +6,7 @@ $dsn = 'heroku pg:psql postgresql-asymmetrical-84387 --app mytodolistalexmelano'
 $userName = 'grnjfagofhvxvc';
 $password = '94326f1215760c1133830addb98d6b2a073a97f9e3842c2d013007b0e90f28b0';
 
-//local db access
-//$dsn = 'mysql:host=localhost;dbname=gamedb';
-//$userName = 'dasha';
-//$password = '12345';
 
-// exception handling - use a try / catch
 try {
 // instantiates a new pdo - an db object
     $db = new PDO($dsn, $userName, $password);
